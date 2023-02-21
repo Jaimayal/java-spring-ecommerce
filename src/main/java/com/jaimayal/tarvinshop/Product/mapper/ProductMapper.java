7ca0b2b56826e5d125082ac04dff9b87fbc5c071
Jaimayal <jaime.ayalaa.76@gmail.com>
2023-02-20 20:43:56 -0600

    Created ProductMapper using MapStruct

---
+++ src/main/java/com/jaimayal/tarvinshop/Product/mapper/ProductMapper.java
@@ -0,0 +1,23 @@
+package com.jaimayal.tarvinshop.Product.mapper;
+
+import com.jaimayal.tarvinshop.Product.dto.ProductDTO;
+import com.jaimayal.tarvinshop.Product.entity.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(
+        componentModel = "spring", 
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public interface ProductMapper {
+    ProductMapper mapper = Mappers.getMapper(ProductMapper.class);
+
+    ProductDTO mapToProductDTO(Product product);
+    Product mapToProduct(ProductDTO productDTO);
+    List<Product> mapToProductList(List<ProductDTO> productDTOList);
+    List<ProductDTO> mapToProductDTOList(List<Product> productList);
+}
