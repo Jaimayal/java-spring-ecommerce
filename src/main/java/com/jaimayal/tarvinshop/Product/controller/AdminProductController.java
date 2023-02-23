ef55a3f2bf2cddffc83f8d1081206a19b81d61bc
Jaimayal <jaime.ayalaa.76@gmail.com>
2023-02-22 20:33:05 -0600

    Changed HTTP verb in update endpoint from PATCH to PUT

--- src/main/java/com/jaimayal/tarvinshop/Product/controller/AdminProductController.java
+++ src/main/java/com/jaimayal/tarvinshop/Product/controller/AdminProductController.java
@@ -8,9 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,7 +43,7 @@ public class AdminProductController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> updateProduct(@PathVariable final Long id,
                                            @RequestBody final ProductDTO productDTO) {
